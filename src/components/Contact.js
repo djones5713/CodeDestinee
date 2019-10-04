157932fe7b55d186882fa1562fd843a2313f7dbb
Destiny Jones <djones5713@gmail.com>
2019-10-04 14:33:57 -0400

    responsive

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar';
 import ContactBox from './ContactBox';
-import Destiny3 from './Images/Destiny3.png';
 import './Contact.css';
 
 
@@ -19,11 +18,8 @@ render(){
             <NavBar />
             <div  className="Contact-Header">
                 <div className="Contact-Content">
+                <h2 className="Contact-Img-Box-Title">Let's Talk Code</h2>
                         <ContactBox />
-                    <div className="Contact-Img-Box">
-                        <h2 className="Contact-Img-Box-Title">Let's Talk Code</h2>
-                        <img className="Contact-img" src={Destiny3} alt="Destiny Jones" />
-                    </div>
                 </div>
             </div>
         </div>
